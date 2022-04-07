71df6fe7bb91d811977217af35d562f9f201b54c
nochi62 <pherrerapriv@gmail.com>
2022-04-06 22:08:38 -0400

    validacion de ingreso de datos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const { send } = require('process');
 
 app.listen(3000, () => console.log('Server ON http://localhost:3000'));
 app.use(express.json());
@@ -13,6 +14,24 @@ app.get('/empleados', (req, res) => {
     res.sendFile(`${__dirname}/empleados.json`);
 });
 
+app.use("/empleados", (req, res, next) => {
+    let {rut, nombre, salario, horas_trabajadas} = req.body;
+    if (rut == '') {
+        res.status(400).send("Debe ingresar Rut")
+    } else if (nombre == ''){
+        res.status(400).send("Debe ingresar Nombre")
+    } else if (salario == ''){
+        res.status(400).send("Debe ingresar Salario")
+    } else if (horas_trabajadas == ''){
+        res.status(400).send("Debe ingresar Horas Trabajadas")
+    } else if (isNaN(salario)){
+        res.status(400).send("Debe ingresar un valor numerico a Salario")
+    } else if (isNaN(horas_trabajadas)){
+        res.status(400).send("Debe ingresar un valor numerico a Horas Trabajadas")
+    } else {
+        next()
+    }
+})
 app.post("/empleados", (req, res) => {
     let {rut, nombre, salario, horas_trabajadas} = req.body;
     let contenido = fs.readFileSync(`${__dirname}/empleados.json`, "utf8");
