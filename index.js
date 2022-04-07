00c8b6188c0c99c80c2177ba0c7d005cf11bfcf5
Jedicillo <seron.cesar@gmail.com>
2022-04-06 21:35:14 -0400

    Agregado /empleados para reistrar

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.listen(3000, () => console.log('Server ON http://localhost:3000'));
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
@@ -9,4 +11,14 @@ app.get("/", (req, res) => {
 
 app.get('/empleados', (req, res) => {
     res.sendFile(`${__dirname}/empleados.json`);
+});
+
+app.post("/empleados", (req, res) => {
+    let {rut, nombre, salario, horas_trabajadas} = req.body;
+    let contenido = fs.readFileSync(`${__dirname}/empleados.json`, "utf8");
+    contenido = JSON.parse(contenido);
+    contenido.empleados.push({rut, nombre, salario, horas_trabajadas});
+    contenido = JSON.stringify(contenido, null, 4);
+    fs.writeFileSync(`${__dirname}/empleados.json`, contenido, "utf8");
+    res.send("Empleado registrado con éxito");
 });
